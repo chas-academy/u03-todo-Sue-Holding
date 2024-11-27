8a9d5a9a034b58e675eb569bd58cabe059035dfd
Sue-Holding <sue.holding55@gmail.com>
2024-11-27 20:44:11 +0100

    simple css styling

--- src/index.php
+++ src/index.php
@@ -1,5 +1,52 @@
 <?php
 
-echo "Welcome to the Harry Potter Themed To Do List Web application!!";
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to the Harry Potter Themed To Do List Web application!!</title>
+    <link rel="stylesheet" href="styles/index.css">
+</head>
+
+<body class="bg">
+<div class="grid-container">   
+<img class="bg" src="images/Arrival_at_Hogwarts.jpg" alt="arrival at hogwarts" width="100%">
+
+<header class="header">
+<h1>Welcome to the Harry Potter Themed To Do List Web application!!</h1>
+</header>
+
+<nav class="nav-bar">
+        <a href="#">Dobby's Daily Chores</a>
+        <a href="#">Log In</a>
+</nav>
+
+
+<aside class="today-list">
+    <h2>Today's Magical Activites</h2>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+</aside>
+
+<aside class="sorting-hat">
+    <h2>Sorting Hat</h2>
+    <p>Choose your tasks by House</p>
+</aside>
+
+<section class="create-your-own"></section>
+    <h2>Create your own daily Dobby chores</h2>
+
+</div> 
+<!-- end of grid container -->
+
+</body>
+</html>
